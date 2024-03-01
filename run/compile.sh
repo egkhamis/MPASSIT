b5af782105950e8908832ef851a4bc3d2f762c7f
egkhamis <egkhamis@gmail.com>
2024-03-01 07:13:38 -0300

    moving files to run dir

--- run/compile.sh
+++ run/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -fr ./build
-mkdir ./build && cd ./build
+rm -fr ../build
+mkdir ../build && cd ../build
 
 cmake .. ${CMAKE_FLAGS}
 
@@ -9,5 +9,5 @@ make -j 8 VERBOSE=1
 
 cp mpassit ../run/
 
-cd ..
+cd ../run
 
